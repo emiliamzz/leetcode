f72780e0cf6e92b82f273ea03451a68dd20ba92b
emiliamzz <emiliamzz@hotmail.com>
2026-04-13 16:31:33 -0400

    383

---
+++ 383. Ransom Note.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for letter in set(ransomNote):
+            if ransomNote.count(letter) > magazine.count(letter):
+                return False
+        return True
