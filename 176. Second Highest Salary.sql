98f64d984810e2a206ffa0a8fd61cbb9f0fb322e
emiliamzz <emiliamzz@hotmail.com>
2026-05-21 19:07:54 -0400

    176

---
+++ 176. Second Highest Salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT (
+    SELECT salary
+    FROM Employee
+    GROUP BY salary
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary
