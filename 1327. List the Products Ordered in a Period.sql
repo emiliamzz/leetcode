dcbcc6bcc1a164e656476a4662e2e7d6969e148b
emiliamzz <emiliamzz@hotmail.com>
2026-05-25 13:25:33 -0400

    1327

---
+++ 1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT product_name,
+    SUM(unit) AS unit
+FROM Products p JOIN Orders o
+    ON p.product_id = o.product_id
+WHERE order_date LIKE '2020-02-%'
+GROUP BY product_name
+HAVING unit >= 100
